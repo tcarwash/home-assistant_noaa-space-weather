cfff7391ce4c5b1c911fc3931da7ac1b6acfe86c
Tyler Carr <tcarwash@gmail.com>
2024-11-01 13:02:58 -0700

    Fixes #53 (#58)

--- custom_components/noaa_space_weather/__init__.py
+++ custom_components/noaa_space_weather/__init__.py
@@ -52,12 +52,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
         entry.add_update_listener(async_reload_entry)
 
     return True
