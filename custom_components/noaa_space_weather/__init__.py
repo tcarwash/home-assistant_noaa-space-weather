a0b410f2e02ff9052539a29f6062011bb3a9db89
Tyler F. Carr <tcarwash@gmail.com>
2026-01-10 23:02:34 +0000

    re-fix async_forward_entry_setup(s)

--- custom_components/noaa_space_weather/__init__.py
+++ custom_components/noaa_space_weather/__init__.py
@@ -52,7 +52,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
